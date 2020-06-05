d719b7507019fa00466231d2748ea8ca8ec9968c
kaceyb <kacey.bennett@gmail.com>
2020-06-05 18:13:02 -0500

    another try

--- algorithmDataStructures/telephoneNumberValidator/app.js
+++ algorithmDataStructures/telephoneNumberValidator/app.js
@@ -6,25 +6,25 @@ function telephoneCheck(str) {
   let hasPermittedCharsOnly = false;
   let hasCorrectParentheses = false;
 
-  let reTen = /\d{10}/;
-    console.log('TenDigitTest');
-    console.log(reTen.test(str));
+  // let reTen = /\d{10}/;
+  //   console.log('TenDigitTest');
+  //   console.log(reTen.test(str));
 
-  let reEleven = /\d{11}/;
-    console.log('ElevenDigitTest');
-    console.log(reEleven.test(str));
+  // let reEleven = /\d{11}/;
+  //   console.log('ElevenDigitTest');
+  //   console.log(reEleven.test(str));
 
-  let reStartOne = /^[1]/;
-    console.log('StartsWithOneTest');
-    console.log(reStartOne.test(str));
+  // let reStartOne = /^[1]/;
+  //   console.log('StartsWithOneTest');
+  //   console.log(reStartOne.test(str));
 
-  let rePermitted = /\d+|\s|\(|\)|\-[^a-zA-Z]/;
+  let rePermitted = /([^a-zA-Z]-|\s|\(|\)|\d+)/g;
     console.log('PermittedTest');
     console.log(rePermitted.test(str));
 
     //return true;
   }
   
-  // telephoneCheck("a555-555-5555");
-  // telephoneCheck("5555555555");
+  telephoneCheck("555-555-5555");
+  telephoneCheck("5555555555");
   telephoneCheck("15b55555");
