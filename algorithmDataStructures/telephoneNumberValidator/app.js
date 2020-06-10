20bf137e60088ecfa5d28683e4aa5473ed308d1d
kaceyb <kacey.bennett@gmail.com>
2020-06-10 17:29:31 -0500

    update

--- algorithmDataStructures/telephoneNumberValidator/app.js
+++ algorithmDataStructures/telephoneNumberValidator/app.js
@@ -18,9 +18,17 @@ function telephoneCheck(str) {
   //   console.log('StartsWithOneTest');
   //   console.log(reStartOne.test(str));
 
-  let rePermitted = /([^a-zA-Z]-|\s|\(|\)|\d+)/g;
-    console.log('PermittedTest');
-    console.log(rePermitted.test(str));
+  // let rePermitted = /([^a-zA-Z]|-|\s|\(|\)|\d+)/g;
+  //   console.log('PermittedTest');
+  //   console.log(rePermitted.test(str));
+
+  // let rePermitted = /([a-zA-Z])/;
+  //   console.log('PermittedTest');
+  //   console.log(rePermitted.test(str));
+
+  let reParenth = /1?\s?\(?\d{3}\)?-?\d{3}-?\d{4}/;
+    console.log('ParenthTest');
+    console.log(reParenth.test(str));
 
     //return true;
   }
@@ -28,3 +36,4 @@ function telephoneCheck(str) {
   telephoneCheck("555-555-5555");
   telephoneCheck("5555555555");
   telephoneCheck("15b55555");
+  telephoneCheck("1 (555)-555-5555");
---
+++ algorithmDataStructures/telephoneNumberValidator/app2.js
@@ -0,0 +1,10 @@
+function telephoneCheck(str) {
+    // let regex = /^(1\s)?(\({1}\d{3}\){1})|(\d{3})-?\s?(\d{4})/;
+    let regex = /^(1\s)?(\({1}\d{3}\){1})|(\d{3})-?|\s?(\d{3})-?|\s?(\d{4})/;
+    console.log(regex.test(str));
+    return regex.test(str);
+  }
+  
+  telephoneCheck("555-555-5555");
+  telephoneCheck("1 (555) 555-5555")
+  telephoneCheck("1 555)555-5555")
