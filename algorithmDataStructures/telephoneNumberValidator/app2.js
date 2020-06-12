53f26bbadf65c63385cdab76420c66cc6ac88393
kaceyb <kacey.bennett@gmail.com>
2020-06-11 21:59:22 -0500

    Working finally!

--- algorithmDataStructures/telephoneNumberValidator/app2.js
+++ algorithmDataStructures/telephoneNumberValidator/app2.js
@@ -1,6 +1,8 @@
 function telephoneCheck(str) {
     // let regex = /^(1\s)?(\({1}\d{3}\){1})|(\d{3})-?\s?(\d{4})/;
-    let regex = /^(1\s)?(\({1}\d{3}\){1})|(\d{3})-?|\s?(\d{3})-?|\s?(\d{4})/;
+    // let regex = /^(1\s)?(\({1}\d{3}\){1})|(\d{3})-?|\s?(\d{3})-?|\s?(\d{4})/;
+    // let regex = /^(1\s)?(\(\d{3}\)\s?)|(\d{3}-?|\s?)(\d{3})-?|\s?(\d{4})/;
+    let regex = /^(1\s?)?(\(\d{3}\)\s?|\d{3})[-\s]?(\d{3})[-\s]?\d{4}$/;
     console.log(regex.test(str));
     return regex.test(str);
   }
